d292b5a98d8c63f082210218264766482855c859
ostapkharysh <harysh@ucu.edu.ua>
2018-04-18 11:10:44 +0300

    update

--- try2.py
+++ try2.py
@@ -1,15 +1,22 @@
-
+import getopt
 from multiprocessing import Pool, Manager, Lock
 import multiprocessing
-import os
+import os, csv
 
+import sys
 
 
 def file_reader(fl):
     data = list()
-    with open(fl, 'r') as file:
-        for line in file:
-            data +=line.strip().lower().split(' ')
+    if fl.endswith('.csv'):
+        with open(fl, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in spamreader:
+                data += (', '.join(row))
+    else:
+        with open(fl, 'r') as file:
+            for line in file:
+                data +=line.strip().lower().split(' ')
     return data
 
 def remove_file(fl):
@@ -95,11 +102,12 @@ def reducer(shuf_file):
             pass
             F.write(data[0][0]+','+str(sum([int(el[1]) for el in data])))
 
-def outer(files):
-    with open("WordsCount.txt", 'a') as fl:
+def outer(files, outputfile):
+    with open(outputfile, 'a') as fl:
         for file in files:
             with open('ReducersData/'+file) as source:
                 fl.write(source.readline() +'\n')
+    print("Words counted in this document are here: "+outputfile)
 
 
 def divide(seq, num):
@@ -113,10 +121,91 @@ def divide(seq, num):
 
     return out
 
+def map_reduce( infile, outfile, mappers_number, reducers_number, combine='y'):
+    #infile = 'data.txt'
+
+    os.mkdir("CombinersData")
+    os.mkdir("MappersData")
+    os.mkdir("Data")
+    os.mkdir("ReducersData")
+
+    out_lines = file_reader(infile)
+    # print(out_lines)
+    if mappers_number is not int:
+        mappers_number=len(out_lines)
+
+    division = divide(out_lines, mappers_number)
+    print(division)
+    # print(out_lines)
+
+    pool = Pool(processes=mappers_number, )
+    #mapped_words = \
+    pool.map(mapper, division)
+
+    file_lst = os.listdir("MappersData")
+    #combine_words =
+    print(combine)
+    if combine !='n':
+        pool.map(combiner, file_lst)
+        shuffler("CombinersData")
+    else:
+        shuffler("MappersData")
+
+    reduce_lst = os.listdir("Data")
+    if reducers_number is not int:
+        reducers_number = len(reduce_lst)
+
+    pool2 = Pool(processes=reducers_number, )
+
+    print(reduce_lst)
+    pool2.map(reducer, reduce_lst)
+
+    outer(os.listdir("ReducersData"), outfile)
+
+#map_reduce()
+def run(argv):
+    mappers_number = '(Not specified) Will depend on the input file size'
+    reducer_number = '(Not specified) Will depend on the input file size'
+    combine = '(Not specified) yes'
+    inputfile = 'file.<txt, csv>'
+    outputfile = 'CountedWords.txt'
+
+    try:
+        opts, args = getopt.getopt(argv, "i:o:m:r:c", [ "inputfile=","outputfile=" "mappers=", "reducers=","combine="])
+    except getopt.GetoptError:
+        print('-i <inputfile<csv,txt>> -o <outputfile<.txt>> -m <mappers number> -r <reducers number> --c <combine?<y,n>>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('-i <inputfile<csv,txt>> -o <outputfile<.txt>> -m <mappers number> -r <reducers number> --c <combine?<y,n>>')
+            sys.exit()
+        elif opt == '-i':
+            inputfile = arg
+        elif opt == '-o':
+            outputfile = arg
+        elif opt in ("-m", "--mappers"):
+            mappers_number = int(arg)
+        elif opt in ("-r", "--reducers"):
+            reducer_number = int(arg)
+        elif opt in ('--c', "--combine"):
+            combine = arg
+        if inputfile == 'file.<txt, csv>':
+            print("Input file should be specified: -i file.<txt, csv>")
+            sys.exit()
+    print('Input file: ', inputfile)
+    print('Output file: ', outputfile)
+    print('Number of mappers: ', mappers_number)
+    print('Number of reducers: ', reducer_number)
+    print('Combine: ', combine)
+    map_reduce(inputfile, outputfile, mappers_number, reducer_number, combine)
 
 
-file = 'data.txt'
+if __name__=="__main__":
+    run(sys.argv[1:])
+    #map_reduce()
 
+"""
+file = 'test.csv'
 os.mkdir("CombinersData")
 os.mkdir("MappersData")
 os.mkdir("Data")
@@ -154,3 +243,5 @@ print(reduce_lst)
 pool2.map(reducer,reduce_lst)
 
 outer(os.listdir("ReducersData"))
+
+"""
